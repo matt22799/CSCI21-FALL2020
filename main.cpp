1112d4d80bfac2465a074f044d9e8da19b694e43
matt2271999 <matt2271999@hotmail.com>
2020-08-30 15:22:03 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,74 @@
+// ASSIGNMENT NAME: Lab 1.2
+// PROGRAMMER NAME
+//
+// DATE
+#include <iostream>
+
+// using namespace std;
+// uncomment the above "using" directive and you can write:
+//   cout instead of std::cout
+//   cin  instead of std::cin
+//   endl instead of std::endl
+
+// QUESTIONs do not have to be answered -- they are there for your consideration.
+
+int main() {
+  // 1. Print a greeting
+
+  // 2. Declare 2 integer variables
+
+  // 3. Prompt for the first integer
+  // 4. Read in the first integer
+
+  // 5. Prompt for the second integer
+  // 6. Read in the second integer
+
+  // 7. Display the result of adding the two variables together
+  //    e.g., 3 + 2 = 5 (use the two variables and the calculated value in the output)
+
+  // 8. Display the result of subtracting the second integer from the first
+  //    e.g., 3 - 2 = 1 (use the two variables and the calculated value in the output)
+
+  // 9. Display the result of multiplying the two variables
+  //    e.g., 3 * 2 = 6 (use the two variables and the calculated value in the output)
+
+  // 10. Display the result of dividing the first integer by the second
+  //    e.g., 3 / 2 = 1 (use the two variables and the calculated value in the output)
+
+  // 11. Display the result of modding (%) the first integer by the second
+  //    e.g., 3 % 2 = 1 (use the two variables and the calculated value in the output)
+  //
+  //    QUESTION: What does modulo/mod (%) do?
+
+  // 12. Declare 2 float variables
+
+  // 13. Prompt for the first float
+  // 14. Read in the first float
+
+  // 15. Prompt for the second float
+  // 16. Read in the second float
+
+  // This code will "pretty print" the float variables.
+  // Try changing the "2" to other values to see what happens.
+  // If you did not uncomment "using" above, you need to use std::cout
+  //   on all three of these lines.
+  cout.setf(ios::fixed);
+  cout.setf(ios::showpoint);
+  cout.precision(2);
+
+  // 17. Display the result of adding the two variables together
+  //    e.g., 3.20 + 2.10 = 5.30 (use the two variables and the calculated value in the output)
+
+  // 18. Display the result of subtracting the second float from the first
+  //    e.g., 3.20 - 2.10 = 1.10 (use the two variables and the calculated value in the output)
+
+  // 19. Display the result of multiplying the two variables
+  //    e.g., 3.20 * 2.10 = 6.72 (use the two variables and the calculated value in the output)
+
+  // 20. Display the result of dividing the first float by the second
+  //    e.g., 3.20 / 2.10 = 1.52 (use the two variables and the calculated value in the output)
+
+  // QUESTION: Why is there no modulo (%) operation required for the float variables?
+
+  return 0;
+}
